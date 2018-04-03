d2eeabc1e869da4b21a13be1813d91d785b6fc44
lbllol365 <1217584374@qq.com>
2018-04-03 17:15:53 +0800

    虚类练习

---
+++ CoreJava/abstractClasses/Student.java
@@ -0,0 +1,14 @@
+package CoreJava.abstractClasses;
+
+public class Student extends Person {
+    private String major;
+    public Student(String name,String major){
+        super(name);
+        this.major = major;
+    }
+
+    public String getDescription(){
+        return "a student maioring in " + major;
+    }
+
+}
