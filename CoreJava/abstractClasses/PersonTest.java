681f5d4d6d8867660977a61a44b6aabe90326024
lbllol365 <1217584374@qq.com>
2018-04-03 17:22:01 +0800

    虚类练习

--- CoreJava/abstractClasses/PersonTest.java
+++ CoreJava/abstractClasses/PersonTest.java
@@ -2,6 +2,12 @@ package CoreJava.abstractClasses;
 
 public class PersonTest {
     public static void main(String[] args){
+        Person[] people = new Person[2];
 
+        people[0] = new Employee("Harry Hacker",50000,1989,10,1);
+        people[1] = new Student("Mary","CS");
+
+        for(Person p:people)
+            System.out.println(p.getName() + " ," + p.getDescription());
     }
 }
